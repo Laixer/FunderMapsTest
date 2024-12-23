d9967e9d2d1faefd79ad53de281edb6102248eab
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-12-23 20:24:47 +0100

    Refactor AuthMiddleware to remove commented-out code and streamline logic; improve code clarity and maintainability

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -7,21 +7,14 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 
-	"fundermaps/internal/config"
 	"fundermaps/internal/database"
 )
-
-// const (
-// 	AuthProviderJWT      = "jwt"
-// 	AuthProviderAPIToken = "api_token"
-// )
 
 const (
 	HeaderXAPIKey = "X-API-Key"
 )
 
 func AuthMiddleware(c *fiber.Ctx) error {
-	cfg := c.Locals("config").(*config.Config)
 	db := c.Locals("db").(*gorm.DB)
 
 	xAPIKey := c.Get(HeaderXAPIKey)
@@ -38,11 +31,7 @@ func AuthMiddleware(c *fiber.Ctx) error {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Internal server error"})
 		}
 
-		// TODO: Move into database stored procedure
-		db.Exec("INSERT INTO application.auth_session (user_id, ip_address, application_id, provider, updated_at) VALUES (?, ?, ?, 'api_token', now()) ON CONFLICT ON constraint auth_session_pkey DO UPDATE SET updated_at = excluded.updated_at, ip_address = excluded.ip_address;", user.ID, c.IP(), cfg.ApplicationID)
-
 		// TODO: Fetch organization and organization role
-		// c.Locals("auth_provider", AuthProviderAPIToken)
 		c.Locals("user", user)
 
 		return c.Next()
@@ -72,46 +61,8 @@ func AuthMiddleware(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Internal server error"})
 	}
 
-	// var accessToken database.AuthAccessToken
-	// result = db.First(&accessToken, "access_token = ?", token)
-	// if result.Error != nil {
-	// 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-	// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized"})
-	// 	}
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Internal server error"})
-	// }
-
-	// accessToken.UpdatedAt = time.Now()
-	// if err := db.Save(&accessToken).Error; err != nil {
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Internal server error"})
-	// }
-
 	// TODO: Fetch organization and organization role
 	c.Locals("user", user)
-
-	// return c.Next()
-	// }
-
-	// claims, err := auth.VerifyJWT(token)
-	// if err != nil {
-	// 	return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized"})
-	// }
-
-	// var user database.User
-	// result := db.Model(&database.User{}).Where("id = ?", claims["id"]).Preload("Organizations").Find(&user)
-	// if result.Error != nil {
-	// 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-	// 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized"})
-	// 	}
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Internal server error"})
-	// }
-
-	// // TODO: Move into database stored procedure
-	// db.Exec("INSERT INTO application.auth_session (user_id, ip_address, application_id, provider, updated_at) VALUES (?, ?, ?, 'jwt', now()) ON CONFLICT ON constraint auth_session_pkey DO UPDATE SET updated_at = excluded.updated_at, ip_address = excluded.ip_address;", user.ID, c.IP(), cfg.ApplicationID)
-
-	// // TODO: Fetch organization and organization role
-	// c.Locals("auth_provider", AuthProviderJWT)
-	// c.Locals("user", user)
 
 	return c.Next()
 }
