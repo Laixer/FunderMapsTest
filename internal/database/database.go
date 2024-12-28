47c414ee2386d7bc7df97c283e86ebc6ae691d73
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-12-28 20:52:27 +0100

    Configure GORM logger to log errors only for improved database connection diagnostics

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,12 +4,15 @@ import (
 	"github.com/gofiber/fiber/v2/log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 
 	"fundermaps/internal/config"
 )
 
 func Connect(c *config.Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(c.DatabaseURL), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(c.DatabaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Error),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
