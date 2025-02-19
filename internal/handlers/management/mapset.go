c19020e0f5feed7e7e8b9563411b159882fb9347
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-02-19 10:07:45 +0000

    Fix GetMapsetByID query to use parameterized SQL for improved safety

--- internal/handlers/management/mapset.go
+++ internal/handlers/management/mapset.go
@@ -23,7 +23,7 @@ func GetMapsetByID(c *fiber.Ctx) error {
 	db := c.Locals("db").(*gorm.DB)
 
 	var mapset database.Mapset
-	result := db.First(&mapset, c.Params("id"))
+	result := db.First(&mapset, "id = ?", c.Params("id"))
 	if result.Error != nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "Mapset not found"})
 	}
