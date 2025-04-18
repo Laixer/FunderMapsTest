a5927ee87d9e7678bf016f2ef74cdbf6a8e27ba3
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-04-18 22:21:26 +0200

    Remove commented-out mail handler code from diag.go

--- app/handlers/diag.go
+++ app/handlers/diag.go
@@ -9,34 +9,3 @@ func GetIP(c *fiber.Ctx) error {
 func GetHeaders(c *fiber.Ctx) error {
 	return c.JSON(c.GetReqHeaders())
 }
-
-// diag.Post("/mail", func(c *fiber.Ctx) error {
-// 	type EmailInput struct {
-// 		Subject string `json:"subject" validate:"required"`
-// 		Body    string `json:"body" validate:"required"`
-// 		From    string `json:"from" validate:"required"`
-// 		To      string `json:"to" validate:"required"`
-// 	}
-
-// 	var input EmailInput
-// 	if err := c.BodyParser(&input); err != nil {
-// 		return c.SendStatus(fiber.StatusBadRequest)
-// 	}
-
-// 	err := config.Validate.Struct(input)
-// 	if err != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": err.Error()})
-// 	}
-
-// 	message := mail.Email{
-// 		Subject: input.Subject,
-// 		Body:    input.Body,
-// 		From:    input.From,
-// 		To:      []string{input.To},
-// 	}
-
-// 	mailer := mail.NewMailer(cfg.MailgunDomain, cfg.MailgunAPIKey, cfg.MailgunAPIBase)
-// 	mailer.SendMail(&message)
-
-// 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "Message queued"})
-// })
