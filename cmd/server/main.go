022d711334293c55a9a99c770d58e64c605ba8c5
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-04-19 10:52:20 +0200

    Refactor logger middleware: Remove duplicate latency from log format

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -68,7 +68,7 @@ func main() {
 	})
 
 	app.Use(logger.New(logger.Config{
-		Format: "${latency} | ${status} | ${latency} | ${method} | ${path}\n",
+		Format: "${latency} | ${status} | ${method} | ${path}\n",
 	}))
 
 	app.Get("/auth/login", func(c *fiber.Ctx) error {
