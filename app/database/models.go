ef088fd4c192ae5607dab556dbe9e79e5f161e5d
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-04-21 14:34:19 +0200

    Refactor Analysis model: Update JSON field names to camelCase for consistency

--- app/database/models.go
+++ app/database/models.go
@@ -30,35 +30,37 @@ func (a *Address) TableName() string {
 	return "geocoder.address"
 }
 
+// TODO: The JSON name should be the same as the database column name, however
+// we use camelCase only here to avoid breaking changes in the API.
 // TODO: Add custom types for the database data types
 type Analysis struct {
-	BuildingID                  string   `json:"building_id" gorm:"->"`
-	NeighborhoodID              string   `json:"neighborhood_id" gorm:"->"`
-	ConstructionYear            *int     `json:"construction_year" gorm:"->"`
-	ConstructionYearReliability string   `json:"construction_year_reliability" gorm:"->"`
-	RecoveryType                *string  `json:"recovery_type" gorm:"->"`
-	RestorationCosts            *float64 `json:"restoration_costs" gorm:"->"`
+	BuildingID                  string   `json:"buildingId" gorm:"->"`
+	NeighborhoodID              string   `json:"neighborhoodId" gorm:"->"`
+	ConstructionYear            *int     `json:"constructionYear" gorm:"->"`
+	ConstructionYearReliability string   `json:"constructionYearReliability" gorm:"->"`
+	RecoveryType                *string  `json:"recoveryType" gorm:"->"`
+	RestorationCosts            *float64 `json:"restorationCosts" gorm:"->"`
 	Height                      *float64 `json:"height" gorm:"->"`
 	Velocity                    *float64 `json:"velocity" gorm:"->"`
-	GroundWaterLevel            *float64 `json:"ground_water_level" gorm:"->"`
-	GroundLevel                 *float64 `json:"ground_level" gorm:"->"`
+	GroundWaterLevel            *float64 `json:"groundWaterLevel" gorm:"->"`
+	GroundLevel                 *float64 `json:"groundLevel" gorm:"->"`
 	Soil                        *string  `json:"soil" gorm:"->"`
-	SurfaceArea                 *float64 `json:"surface_area" gorm:"->"`
-	DamageCause                 *string  `json:"damage_cause" gorm:"->"`
-	EnforcementTerm             *string  `json:"enforcement_term" gorm:"->"`
-	OverallQuality              *string  `json:"overall_quality" gorm:"->"`
-	InquiryType                 *string  `json:"inquiry_type" gorm:"->"`
-	FoundationType              *string  `json:"foundation_type" gorm:"->"`
-	FoundationTypeReliability   string   `json:"foundation_type_reliability" gorm:"->"`
+	SurfaceArea                 *float64 `json:"surfaceArea" gorm:"->"`
+	DamageCause                 *string  `json:"damageCause" gorm:"->"`
+	EnforcementTerm             *string  `json:"enforcementTerm" gorm:"->"`
+	OverallQuality              *string  `json:"overallQuality" gorm:"->"`
+	InquiryType                 *string  `json:"inquiryType" gorm:"->"`
+	FoundationType              *string  `json:"foundationType" gorm:"->"`
+	FoundationTypeReliability   string   `json:"foundationTypeReliability" gorm:"->"`
 	Drystand                    *float64 `json:"drystand" gorm:"->"`
-	DrystandReliability         string   `json:"drystand_reliability" gorm:"->"`
-	DrystandRisk                *string  `json:"drystand_risk" gorm:"->"`
+	DrystandReliability         string   `json:"drystandReliability" gorm:"->"`
+	DrystandRisk                *string  `json:"drystandRisk" gorm:"->"`
 	DewateringDepth             *float64 `json:"dewatering_depth" gorm:"->"`
-	DewateringDepthReliability  string   `json:"dewatering_depth_reliability" gorm:"->"`
-	DewateringDepthRisk         *string  `json:"dewatering_depth_risk" gorm:"->"`
-	BioInfectionReliability     string   `json:"bio_infection_reliability" gorm:"->"`
-	BioInfectionRisk            *string  `json:"bio_infection_risk" gorm:"->"`
-	UnclassifiedRisk            *string  `json:"unclassified_risk" gorm:"->"`
+	DewateringDepthReliability  string   `json:"dewateringDepthReliability" gorm:"->"`
+	DewateringDepthRisk         *string  `json:"dewateringDepthRisk" gorm:"->"`
+	BioInfectionReliability     string   `json:"bioInfectionReliability" gorm:"->"`
+	BioInfectionRisk            *string  `json:"bioInfectionRisk" gorm:"->"`
+	UnclassifiedRisk            *string  `json:"unclassifiedRisk" gorm:"->"`
 }
 
 func (a *Analysis) TableName() string {
@@ -110,16 +112,6 @@ type Organization struct {
 func (o *Organization) TableName() string {
 	return "application.organization"
 }
-
-// type OrganizationUser struct {
-// 	UserID         uuid.UUID `gorm:"type:uuid;primaryKey"`
-// 	OrganizationID uuid.UUID `gorm:"type:uuid;primaryKey"`
-// 	Role           string    `gorm:"not null"`
-// }
-
-// func (ou *OrganizationUser) TableName() string {
-// 	return "application.organization_user"
-// }
 
 // Application represents an OAuth application that can access the API
 type Application struct {
--- app/handlers/analysis.go
+++ app/handlers/analysis.go
@@ -15,7 +15,8 @@ func GetAnalysis(c *fiber.Ctx) error {
 	buildingID := c.Params("building_id")
 
 	var analysis database.Analysis
-	result := db.First(&analysis, "external_building_id = ?", buildingID)
+	result := db.Select("external_building_id AS building_id, neighborhood_id, construction_year, construction_year_reliability, foundation_type, foundation_type_reliability, restoration_costs, drystand, drystand_risk, drystand_risk_reliability, bio_infection_risk, bio_infection_risk_reliability, dewatering_depth, dewatering_depth_risk, dewatering_depth_risk_reliability, unclassified_risk, height, velocity, ground_water_level, ground_level, soil, surface_area, owner, inquiry_id, inquiry_type, damage_cause, enforcement_term, overall_quality, recovery_type").
+		First(&analysis, "external_building_id = ?", buildingID)
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "Analysis not found"})
