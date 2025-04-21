da7bd547f3509e6087a8156607fe2e8402aa633d
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-04-21 12:15:09 +0200

    Refactor JSON handling in database types: Improve error handling and streamline Scan and Value methods for StringArray, JSONObject, and JSONArray

--- app/database/types.go
+++ app/database/types.go
@@ -3,7 +3,6 @@ package database
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"strings"
 )
@@ -17,15 +16,15 @@ func (a *StringArray) Scan(value any) error {
 	}
 
 	switch v := value.(type) {
-	// case []byte:
-	// 	str := string(v)
-	// 	str = strings.Trim(str, "{}")
-	// 	if str == "" {
-	// 		*a = []string{}
-	// 	} else {
-	// 		*a = strings.Split(str, ",")
-	// 	}
-	// 	return nil
+	case []byte:
+		str := string(v)
+		str = strings.Trim(str, "{}")
+		if str == "" {
+			*a = []string{}
+		} else {
+			*a = strings.Split(str, ",")
+		}
+		return nil
 	case string:
 		str := strings.Trim(v, "{}")
 		if str == "" {
@@ -60,20 +59,34 @@ func (j *JSONObject) Scan(value any) error {
 		return nil
 	}
 
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	var bytes []byte
+	switch v := value.(type) {
+	case []byte:
+		bytes = v
+	case string:
+		bytes = []byte(v)
+	default:
+		return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type JSONObject", value)
 	}
 
 	var result map[string]any
 	err := json.Unmarshal(bytes, &result)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal JSONObject: %w", err)
+	}
 	*j = JSONObject(result)
-	return err
+	return nil
 }
 
 func (j JSONObject) Value() (driver.Value, error) {
-	valueString, err := json.Marshal(j)
-	return string(valueString), err
+	if len(j) == 0 {
+		return nil, nil
+	}
+	valueBytes, err := json.Marshal(j)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal JSONObject: %w", err)
+	}
+	return string(valueBytes), nil
 }
 
 type JSONArray []any
@@ -84,18 +97,32 @@ func (a *JSONArray) Scan(value any) error {
 		return nil
 	}
 
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	var bytes []byte
+	switch v := value.(type) {
+	case []byte:
+		bytes = v
+	case string:
+		bytes = []byte(v)
+	default:
+		return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type JSONArray", value)
 	}
 
-	var result []interface{}
+	var result []any
 	err := json.Unmarshal(bytes, &result)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal JSONArray: %w", err)
+	}
 	*a = JSONArray(result)
-	return err
+	return nil
 }
 
 func (a JSONArray) Value() (driver.Value, error) {
-	valueString, err := json.Marshal(a)
-	return string(valueString), err
+	if len(a) == 0 {
+		return nil, nil
+	}
+	valueBytes, err := json.Marshal(a)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal JSONArray: %w", err)
+	}
+	return string(valueBytes), nil
 }
